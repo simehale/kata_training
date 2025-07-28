6c8f2e22c2c66a1bddca5a532afa84167cd53119
simehale <simeonhaley@yeamotion.design>
2025-07-28 11:32:01 -0500

    figure out string storage

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-const newString = "";
 
 function dashToCamel(phrase) { 
 let words = phrase.split("-");      
 words.forEach((word, index) => {
-    if (index == 0) {
-        return word;
-    } else {
-        toUpperCase(word);
+    if (index > 0) {
+        toUpperCase(word[0]);
     }
-            
+      const newstring = word   
     });
+    words.join(word)
 }
