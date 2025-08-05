b9d6ae538eead569b72be5f4b28c30cb18334146
simehale <simeonhaley@yeamotion.design>
2025-08-05 11:09:06 -0500

    correct arguments

--- index.js
+++ index.js
@@ -16,10 +16,14 @@ console.log(dashToCamel("the-stealth-warrior"));
 
 console.log("challenge #2");
 function reverseWords(string) {
- let words = string.split(" ");//split(string)
- words.forEach((word, index, words) => {
- let char = words.split();//split(individual words). creates array of characters inside each word object 
-  char.reverse(word);//reverse characters
+ let words = string.split(" ");
+ words.forEach((word, index) => {
+ let char = word.split();
+  char.reverse() + char.join();
+});
+return words();
 }
+
+console.log(reverseWords("the stealth warrior"))
 //join each character array with join()
 //join each word with join(" "). result: eht htlaets roirraw
